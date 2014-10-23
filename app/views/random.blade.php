87ae747caa11d9ebdb6484fcc3cf1f77ed812706
masbaty <masbaty@gmail.com>
2014-10-22 23:57:41 -0400

    Started working on random users form. Also added link to index.

--- app/views/random.blade.php
+++ app/views/random.blade.php
@@ -9,6 +9,35 @@
 
 @section("content")
 
+<a href="..">Back to main page</a>
 
+<h1>Random User Generator</h1>
+
+How many users? 
+
+<form method='POST'>
+		<label for='users'>How many users? </label>
+			<input type='text' name='users' id='users' value='' />
+
+		<label for='birthdate'>Birthdate </label>
+			<input type="checkbox" name='birthdate' id='birthdate' />
+
+		<label for='profile'>Profile </label>
+			<input type="checkbox" name='profile' id='profile' />
+
+		<input type='submit' value='Generate Users!' />
+
+
+		
+		@if(isset($text))
+			<h2>Here is your Lorem Ipsum text:</h2>
+
+			@foreach($text['paragraphs'] as $paragraph)
+				<p>
+					{{ $paragraph }}
+				</p
+			@endforeach
+		@endif
+</form>
 
 @stop
